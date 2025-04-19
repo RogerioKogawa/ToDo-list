167b2c86d0e9baa813c1c84a613f1e21f11b1fe1
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-18 23:27:17 -0300

    Adicionando construtor e get da classe NovaTarefa

--- src/NovaTarefa.java
+++ src/NovaTarefa.java
@@ -1,2 +1,11 @@
 public class NovaTarefa {
+    private String tarefa;
+
+    public NovaTarefa(String tarefaNova){
+        this.tarefa = tarefaNova;
+    }
+
+    public String getTarefa(){
+        return this.tarefa;
+    }
 }
