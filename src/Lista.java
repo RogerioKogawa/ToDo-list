c6209317cf5342eccbda605175c1f552f58bf9b6
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-18 23:15:14 -0300

    Adicionando o menu de escolha

--- src/Lista.java
+++ src/Lista.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Lista {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in)
+;
+        System.out.println("Escolha uma das opções: ");
+
+        System.out.println("1 - Criar uma nova tarefa");
+        System.out.println("2 - Deletar uma tarefa");
+        System.out.println("3 - Modificar uma tarefa");
+        System.out.println("4 - Marcar uma tarefa como concluída");
+        String opcaoEscolhida = input.nextLine();
+        System.out.println("Você escolheu a opção: " + opcaoEscolhida);
+    }
 }
