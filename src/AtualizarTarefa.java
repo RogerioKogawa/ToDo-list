eb6d7b425401b133b9fce5c8867700be0b30bcb4
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-19 18:31:08 -0300

    Alterando visibilidade de metodo(s)
    Alterando private do metodo modificarTarefa para protected

--- src/AtualizarTarefa.java
+++ src/AtualizarTarefa.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class AtualizarTarefa {
     Scanner input = new Scanner(System.in);
-    private void modificarTarefa(ArrayList<String> listaTarefas, int idTarefa){
+    protected void modificarTarefa(ArrayList<String> listaTarefas, int idTarefa){
         System.out.println("Digite o novo nome da tarefa: ");
         String tarefaModificada = input.nextLine();
         listaTarefas.set(idTarefa, tarefaModificada);
