ebd41037e56f5195941299e7cf81ba2ea791d7ca
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-19 18:12:13 -0300

    Adicionando metodos em ConcluirTarefa Adicionando terminarTarefa Adicionando adicionarListaConcluida

--- src/ConcluirTarefa.java
+++ src/ConcluirTarefa.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class ConcluirTarefa {
+    ArrayList<String> tarefasConcluidas = new ArrayList<String>();
+
+    private void terminarTarefa(ArrayList<String> tarefa, int idTarefa){
+        System.out.println(tarefa.get(idTarefa) + " concluída!");
+        adicionarListaConcluida(tarefa.get(idTarefa));
+    }
+    
+    private void adicionarListaConcluida(String tarefaConcluida){
+        tarefasConcluidas.add(tarefaConcluida);
+
+    }
 }
