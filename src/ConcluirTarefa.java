b7bea55ac0fcad8bfd5b4359cd6011aab169f6ef
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-20 09:54:38 -0300

    Deletando tarefa em Concluir tarefa
    Agora quando uma tarefa é terminada, ela é removida da lista.

--- src/ConcluirTarefa.java
+++ src/ConcluirTarefa.java
@@ -2,14 +2,15 @@ import java.util.ArrayList;
 
 public class ConcluirTarefa {
     ArrayList<String> tarefasConcluidas = new ArrayList<String>();
+    DeletarTarefa deletaTarefa = new DeletarTarefa();
 
     protected void terminarTarefa(ArrayList<String> tarefa, int idTarefa){
         System.out.println(tarefa.get(idTarefa) + " concluída!");
         adicionarListaConcluida(tarefa.get(idTarefa));
+        deletaTarefa.deletarTarefa(tarefa, idTarefa);
     }
 
     protected void adicionarListaConcluida(String tarefaConcluida){
         tarefasConcluidas.add(tarefaConcluida);
-
     }
 }
