c0b67aa197fae451e5b0ad14767e46dca86d8921
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-20 09:52:55 -0300

    Adicionando multiplas requisições

--- src/Lista.java
+++ src/Lista.java
@@ -8,38 +8,41 @@ public class Lista {
     static AtualizarTarefa atualizarTarefa = new AtualizarTarefa();
     static ConcluirTarefa concluirTarefa = new ConcluirTarefa();
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in)
-;
-        System.out.println("Escolha uma das opções: ");
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("1 - Criar uma nova tarefa");
-        System.out.println("2 - Deletar uma tarefa");
-        System.out.println("3 - Modificar uma tarefa");
-        System.out.println("4 - Marcar uma tarefa como concluída");
-        String opcaoEscolhida = input.nextLine();
-        System.out.println("Você escolheu a opção: " + opcaoEscolhida);
+        while(true) {
+            System.out.println("Escolha uma das opções: ");
 
-        switch(Integer.parseInt(opcaoEscolhida)){
-            case 1:
-                System.out.println("Digite a tarefa: ");
-                String tarefa = input.nextLine();
-                novaTarefa.adicionarTarefa(tarefa);
-                break;
-            case 2:
-                System.out.println("Digite o número da tarefa: ");
-                int idTarefaDeletar = Integer.parseInt(input.nextLine());
-                deletaTarefa.deletarTarefa(lista,idTarefaDeletar);
-                break;
-            case 3:
-                System.out.println("Digite o número da tarefa: ");
-                int idTarefaAlterar = Integer.parseInt(input.nextLine());
-                atualizarTarefa.modificarTarefa(lista, idTarefaAlterar);
-                break;
-            case 4:
-                System.out.println("Digite o numero da tarefa");
-                int idTarefaConcluida = Integer.parseInt(input.nextLine());
-                concluirTarefa.terminarTarefa(lista, idTarefaConcluida);
-                break;
+            System.out.println("1 - Criar uma nova tarefa");
+            System.out.println("2 - Deletar uma tarefa");
+            System.out.println("3 - Modificar uma tarefa");
+            System.out.println("4 - Marcar uma tarefa como concluída");
+            String opcaoEscolhida = input.nextLine();
+            System.out.println("Você escolheu a opção: " + opcaoEscolhida);
+
+            switch (Integer.parseInt(opcaoEscolhida)) {
+                case 1:
+                    System.out.println("Digite a tarefa: ");
+                    String tarefa = input.nextLine();
+                    novaTarefa.adicionarTarefa(tarefa);
+                    lista.add(tarefa);
+                    break;
+                case 2:
+                    System.out.println("Digite o número da tarefa: ");
+                    int idTarefaDeletar = Integer.parseInt(input.nextLine());
+                    deletaTarefa.deletarTarefa(lista, idTarefaDeletar);
+                    break;
+                case 3:
+                    System.out.println("Digite o número da tarefa: ");
+                    int idTarefaAlterar = Integer.parseInt(input.nextLine());
+                    atualizarTarefa.modificarTarefa(lista, idTarefaAlterar);
+                    break;
+                case 4:
+                    System.out.println("Digite o numero da tarefa");
+                    int idTarefaConcluida = Integer.parseInt(input.nextLine());
+                    concluirTarefa.terminarTarefa(lista, idTarefaConcluida);
+                    break;
+            }
         }
     }
 }
